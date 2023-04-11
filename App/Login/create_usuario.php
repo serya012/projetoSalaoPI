7ee390ae4059be204f3a67b8a94feda81532c973
PatrickAndradeSC <128238530+PatrickAndradeSC@users.noreply.github.com>
2023-04-11 16:48:25 -0300

    Estilização Login e Cadastro

--- App/Login/create_usuario.php
+++ App/Login/create_usuario.php
@@ -55,65 +55,78 @@ if (isset($_POST["gravar"])){
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/estiloCadastroU.css">
-    <title>Cadastro SóDelas</title>
-</head>
-<body>
-    <div>
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../css/estiloCadastroU.css">
+  <title>Cadastro Teste</title>
+</head>
+
+<body>
+  <div class="container">
+
+    <div class="form-image">
+      <img src="../../img/undraw_woman_ffrd (1).svg" alt="">
     </div>
-    <div class="box">
-        <form action="">
-            <fieldset>
-                <legend><b>Cadastro de Usuário</b></legend>
-                <br>
-                <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser <?php echo (!empty($nome_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $nome; ?>" required>
-                    <label for="nome" class="labelInput">Nome completo</label>
-                    <span class="invalid-feedback"><?php echo $nome_err; ?></span>
-                </div>
-                <br><br>
-                <div class="inputBox">
-                    <input type="text" name="email" id="email" class="inputUser <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>" required>
-                    <label for="email" class="labelInput">Email</label>
-                    <span class="invalid-feedback"><?php echo $email_err; ?></span>
-                </div>
-                <br><br>
-                <div class="inputBox">
-                    <input type="tel" name="telefone" id="telefone" class="inputUser <?php echo (!empty($telefone_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $telefone; ?>" required>
-                    <label for="telefone" class="labelInput">Telefone</label>
-                    <span class="invalid-feedback"><?php echo $telefone_err; ?></span>
-                </div>
-                <br><br>
-                <div class="inputBox">
-                    <input type="text" name="cpf" id="cpf" class="inputUser <?php echo (!empty($cpf_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $cpf; ?>" required>
-                    <label for="cpf" class="labelInput">Cpf</label>
-                    <span class="invalid-feedback"><?php echo $cpf_err; ?></span>
-                </div>
-                <br><br>
-                <div class="inputBox">
-                    <input type="password" name="senha" id="senha" class="inputUser <?php echo (!empty($senha_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $senha; ?>" required>
-                    <label for="senha" class="labelInput">Senha</label>
-                    <span class="invalid-feedback"><?php echo $senha_err; ?></span>
-                </div>
-                <br><br>
-                <div class="inputBox">
-                    <input type="password" name="confirma" id="confirma" class="inputUser <?php echo (!empty($confirma_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirma; ?>" required>
-                    <label for="confirma" class="labelInput">Confrimar senha</label>
-                    <span class="invalid-feedback"><?php echo $confirma_err; ?></span>
-                </div>
-                <p>Já tem uma conta? <a href="login_usuario.php">Entre aqui</a>.</p>
-                
-                <input type="submit" name="submit" id="submit">
-            </fieldset>
-            
-        </form>
+    <div class="form">
+      <form action="" method="POST">
+        <div class="form-header">
+          <div class="tittle">
+            <h1>Cadastre-se</h1>
+          </div>
+          <div class="login-button">
+            <button><a href="">Entrar</a></button>
+          </div>
+        </div>
+
+        <div class="input-group">
+          <div class="input-box">
+            <label for="nome">Nome Completo</label>
+            <input type="text" id="nome" name="nome" placeholder="Digite seu nome" class="inputUser <?php echo (!empty($nome_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $nome; ?>">
+            <span class="invalid-feedback"><?php echo $nome_err; ?></span>
+          </div>
+
+          <div class="input-box">
+            <label for="email">E-mail</label>
+            <input type="email" id="email" name="email" placeholder="Digite seu E-mail" class="inputUser <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+            <span class="invalid-feedback"><?php echo $email_err; ?></span>
+          </div>
+
+          <div class="input-box">
+            <label for="telefone">Telefone</label>
+            <input type="tel" id="telefone" name="telefone" placeholder="(xx) xxxxx-xxxx" class="inputUser <?php echo (!empty($telefone_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $telefone; ?>">
+            <span class="invalid-feedback"><?php echo $telefone_err; ?></span>
+          </div>
+
+          <div class="input-box">
+            <label for="cpf">CPF</label>
+            <input type="text" id="cpf" name="cpf" placeholder="000.000.000-00" class="inputUser <?php echo (!empty($cpf_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $cpf; ?>">
+            <span class="invalid-feedback"><?php echo $cpf_err; ?></span>
+          </div>
+
+          <div class="input-box">
+            <label for="senha">Senha</label>
+            <input type="password" id="senha" name="senha" placeholder="Digite sua senha" class="inputUser <?php echo (!empty($senha_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $senha; ?>">
+            <span class="invalid-feedback"><?php echo $senha_err; ?></span>
+          </div>
+
+          <div class="input-box">
+            <label for="confirma">Confirmar senha</label>
+            <input type="password" id="confirma" name="confirma" placeholder="Confirme sua senha" class="inputUser <?php echo (!empty($confirma_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirma; ?>">
+            <span class="invalid-feedback"><?php echo $confirma_err; ?></span>
+          </div>
+        </div>
+
+        <div class="continue-button">
+          <button><a href="">Continuar</a></button>
+        </div>
+      </form>
     </div>
-    <script type= "text/javascript" src="../../js/jquery-3.6.4.js"></script>
+  </div>
+
+  <script type= "text/javascript" src="../../js/jquery-3.6.4.js"></script>
     <script type= "text/javascript" src="../../js/jquery.mask.js"></script>
     <script type="text/javascript">
        $(document).ready(function(){
@@ -123,4 +136,5 @@ if (isset($_POST["gravar"])){
     </script>
 
 </body>
+
 </html>
