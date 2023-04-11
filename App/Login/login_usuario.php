f3fe026c8dc9b05086774f679db40b0f7d8a168b
PatrickAndradeSC <128238530+PatrickAndradeSC@users.noreply.github.com>
2023-04-10 23:19:35 -0300

    teste

--- App/Login/login_usuario.php
+++ App/Login/login_usuario.php
@@ -46,29 +46,29 @@ if(isset($_POST['buscar'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/estiloLoginU.css">
     <title>Login</title>
 </head>
 <body>
-
-  <div class="container">
-        <h2>Logo</h2>      
-         <form action="" method="post">
-            <div class="form-group">
-                <label>Email: </label>
-                <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
-                <span class="invalid-feedback"><?php echo $email_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Senha:</label>
-                <input type="password" name="senha" class="form-control <?php echo (!empty($senha_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $senha_err; ?></span>
-            </div>
-            <p></p>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login" name="buscar">
-            </div>
-            <p><a href="create_usuario.php">Não tem uma conta? Registre-se aqui!</a></p>
-        </form>
-    </div>    
+<div class="container">
+  <h2>Logo</h2>      
+   <form action="" method="post">
+      <div class="form-group">
+          <label>Email: </label>
+          <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+          <span class="invalid-feedback"><?php echo $email_err; ?></span>
+      </div>    
+      <div class="form-group">
+          <label>Senha:</label>
+          <input type="password" name="senha" class="form-control <?php echo (!empty($senha_err)) ? 'is-invalid' : ''; ?>">
+          <span class="invalid-feedback"><?php echo $senha_err; ?></span>
+      </div>
+      <p></p>
+      <div class="form-group">
+          <input type="submit" class="btn btn-primary" value="Login" name="buscar">
+      </div>
+      <p><a href="create_usuario.php">Não tem uma conta? Registre-se aqui!</a></p>
+  </form>
+</div>    
 </body>
 </html>
